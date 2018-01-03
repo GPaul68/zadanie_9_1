9899377ea1219ea87659571d50c539a3e9146c5a
Paweł Galeja <pgaleja@gmail.com>
2018-01-03 18:55:44 +0100

    Sorting code in js/scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,9 @@
-function getTriangleArea(a, h) {
-    if (a > 0 && h > 0) { return (a*h/2) }  
-    else { return ("Nieprawidłowe dane"); }
+function getTriangleArea(a,h) {
+    if (a>0 && h > 0) {
+        return (a*h/2);
+    } else {
+        return ("Nieprawidłowe dane");
+    }
 }
 
 console.log(getTriangleArea(10, 6));
